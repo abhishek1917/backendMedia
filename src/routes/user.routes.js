9fd55031094934917aaf7d4041a82d93b3ead916
abhishek1917 <abhishek13717@gmail.com>
2024-09-17 17:09:43 +0530

    updated all router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,12 @@
 import {Router} from "express";
-import {loginUser, registerUser,logoutUser,refreshAccessToken} from "../controller/user.controller.js";
+
+
+import {loginUser, registerUser,logoutUser,refreshAccessToken, 
+    changeCurrentPassword, getCurrentUser, updateAccountDetails, 
+    updateUserAvatar, updateUserCoverImage, getUserChannelProfile,
+     getWatchedHistory} from "../controller/user.controller.js";
+
+
 import {upload} from "../middlewares/multer.uploadOnDisk.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -27,6 +34,23 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("./refresh-token").post(refreshAccessToken)
+
+router.route("./change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("./current-user").get(verifyJWT,getCurrentUser)
+
+router.route("./update-accout").patch(verifyJWT,updateAccountDetails)
+
+router.route("./avatra-update").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("./coverImage-update").patch(verifyJWT,upload.single("coverIage"),updateUserCoverImage)
+
+
+router.route("./channel/:username").get(verifyJWT,getUserChannelProfile)
+//yahan jo username hai vo same hona chahiye apne function ka or hum isme : (collans use karenge)
+
+
+router.route("./history").get(verifyJWT,getWatchedHistory)
 
 
 
